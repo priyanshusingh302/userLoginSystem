9863e4456ac54ec1f0b0152522110b44785a9a69
priyanshusingh302 <85619842+priyanshusingh302@users.noreply.github.com>
2022-07-03 10:52:24 +0530

    Update urls.py

--- userLoginSystem/urls.py
+++ userLoginSystem/urls.py
@@ -16,12 +16,10 @@ Including another URLconf
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
-from django.conf.urls import url
-from django.views.static import serve
+# from django.conf.urls import url
+# from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('authentication.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    path('',include('authentication.urls'))
 ]
